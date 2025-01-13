e0f4cded55171df1b649716fcf320d922f10582a
Hunter Haufler <haufhun@gmail.com>
2025-01-12 19:35:16 -0600

    Get Routes and Assets Setup

---
+++ src/app/product/index.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+
+type Props = {};
+
+const Product = (props: Props) => {
+  return (
+    <View>
+      <Text>Product</Text>
+    </View>
+  );
+};
+
+export default Product;
+
+const styles = StyleSheet.create({});
