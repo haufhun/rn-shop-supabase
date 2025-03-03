28c179db812df9122504c607873212b0a8e96591
Hunter Haufler <haufhun@gmail.com>
2025-03-03 07:46:17 -0600

    Setup Supabase. Add AuthProvider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,33 +1,36 @@
 import React from "react";
 import { Stack } from "expo-router";
 import { ToastProvider } from "react-native-toast-notifications";
+import AuthProvider from "../providers/auth-provider";
 
 const RootLayout = () => {
   return (
     <ToastProvider>
-      <Stack>
-        <Stack.Screen
-          name="(shop)"
-          options={{ headerShown: false, title: "Shop" }}
-        />
-        <Stack.Screen
-          name="categories"
-          options={{ headerShown: false, title: "Categories" }}
-        />
-        <Stack.Screen
-          name="product"
-          options={{ headerShown: false, title: "Product" }}
-        />
-        <Stack.Screen
-          name="cart"
-          options={{
-            presentation: "modal",
-            headerShown: true,
-            title: "Shopping Cart",
-          }}
-        />
-        <Stack.Screen name="auth" options={{ headerShown: true }} />
-      </Stack>
+      <AuthProvider>
+        <Stack>
+          <Stack.Screen
+            name="(shop)"
+            options={{ headerShown: false, title: "Shop" }}
+          />
+          <Stack.Screen
+            name="categories"
+            options={{ headerShown: false, title: "Categories" }}
+          />
+          <Stack.Screen
+            name="product"
+            options={{ headerShown: false, title: "Product" }}
+          />
+          <Stack.Screen
+            name="cart"
+            options={{
+              presentation: "modal",
+              headerShown: true,
+              title: "Shopping Cart",
+            }}
+          />
+          <Stack.Screen name="auth" options={{ headerShown: false }} />
+        </Stack>
+      </AuthProvider>
     </ToastProvider>
   );
 };
---
+++ src/providers/auth-provider.tsx
@@ -0,0 +1,66 @@
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { Session } from "@supabase/supabase-js";
+import { supabase } from "../lib/supabase";
+
+type AuthData = {
+  session: Session | null;
+  user: any;
+  mounting: boolean;
+};
+
+const AuthContext = createContext<AuthData>({
+  session: null,
+  user: null,
+  mounting: true,
+});
+
+export default function AuthProvider({ children }: PropsWithChildren) {
+  const [session, setSession] = useState<Session | null>(null);
+  const [user, setUser] = useState(null);
+  const [mounting, setMounting] = useState(true);
+
+  useEffect(() => {
+    const fetchSession = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+
+      setSession(session);
+
+      if (session) {
+        const { data: user, error } = await supabase
+          .from("users")
+          .select("*")
+          .eq("id", session.user.id)
+          .single();
+
+        if (error) {
+          console.error("error", error);
+        } else {
+          setUser(user);
+        }
+      }
+
+      setMounting(false);
+    };
+
+    fetchSession();
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ session, mounting, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => useContext(AuthContext);
