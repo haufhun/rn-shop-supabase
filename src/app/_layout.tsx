0c7afb02926e72d579d813737bca99fca2e8de0b
Hunter Haufler <haufhun@gmail.com>
2025-04-15 11:03:17 -0500

    EAS Setup and Notification Setup and Testing Push NOtification Notification on a Real Device

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,6 +4,7 @@ import { ToastProvider } from "react-native-toast-notifications";
 import AuthProvider from "../providers/auth-provider";
 import QueryProvider from "../providers/query-provider";
 import { StripeProvider } from "@stripe/stripe-react-native";
+import NotificationProvider from "../providers/notification-provider";
 
 const RootLayout = () => {
   return (
@@ -13,29 +14,31 @@ const RootLayout = () => {
           <StripeProvider
             publishableKey={process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY}
           >
-            <Stack>
-              <Stack.Screen
-                name="(shop)"
-                options={{ headerShown: false, title: "Shop" }}
-              />
-              <Stack.Screen
-                name="categories"
-                options={{ headerShown: false, title: "Categories" }}
-              />
-              <Stack.Screen
-                name="product"
-                options={{ headerShown: false, title: "Product" }}
-              />
-              <Stack.Screen
-                name="cart"
-                options={{
-                  presentation: "modal",
-                  headerShown: true,
-                  title: "Shopping Cart",
-                }}
-              />
-              <Stack.Screen name="auth" options={{ headerShown: false }} />
-            </Stack>
+            <NotificationProvider>
+              <Stack>
+                <Stack.Screen
+                  name="(shop)"
+                  options={{ headerShown: false, title: "Shop" }}
+                />
+                <Stack.Screen
+                  name="categories"
+                  options={{ headerShown: false, title: "Categories" }}
+                />
+                <Stack.Screen
+                  name="product"
+                  options={{ headerShown: false, title: "Product" }}
+                />
+                <Stack.Screen
+                  name="cart"
+                  options={{
+                    presentation: "modal",
+                    headerShown: true,
+                    title: "Shopping Cart",
+                  }}
+                />
+                <Stack.Screen name="auth" options={{ headerShown: false }} />
+              </Stack>
+            </NotificationProvider>
           </StripeProvider>
         </QueryProvider>
       </AuthProvider>
---
+++ src/providers/notification-provider.tsx
@@ -0,0 +1,50 @@
+import { useState, useEffect, useRef, PropsWithChildren } from "react";
+import * as Notifications from "expo-notifications";
+import { registerForPushNotificationsAsync } from "../lib/notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+export default function NotificationProvider({ children }: PropsWithChildren) {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState<
+    Notifications.Notification | undefined
+  >(undefined);
+  const notificationListener = useRef<Notifications.EventSubscription>();
+  const responseListener = useRef<Notifications.EventSubscription>();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+      .then((token) => setExpoPushToken(token ?? ""))
+      .catch((error: any) => setExpoPushToken(`${error}`));
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      notificationListener.current &&
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        );
+      responseListener.current &&
+        Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  console.log("expoPushToken", expoPushToken);
+  console.log("notification", notification);
+
+  return <>{children}</>;
+}
